27577c4a5939767be6f67a4b3a9557d081bfb159
Qwexaro <Sergikstar25.gold@mail.ru>
2025-04-08 14:21:57 +0300

    main

--- src/main/java/ElectricDevice.java
+++ src/main/java/ElectricDevice.java
@@ -1,3 +1,16 @@
 public interface ElectricDevice {
 
+    boolean isSwitched();
+
+    void switchOn();
+
+    void switchOff();
+
+    default void toggle() {
+        if (isSwitched()) {
+            switchOff();
+        } else {
+            switchOn();
+        }
+    }
 }
